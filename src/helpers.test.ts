304fb1885ad3f4bfd8754dd944f31a6e1b6a52c8
Tatiana Kudashova <kudashova93@gmail.com>
2020-04-07 10:13:42 +0400

    fix tests

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,5 +1,14 @@
 import { declOfNum } from "./helpers";
 
-test("declension of nouns", () => {
-  expect(declOfNum(1, ["яблоко", "яблока", "яблок"])).toBe("яблоко");
+describe("declension of nouns", () => {
+  it("1 яблоко", () => {
+    expect(declOfNum(1, ["яблоко", "яблока", "яблок"])).toBe("яблоко");
+  });
+  it("3 яблока", () => {
+    expect(declOfNum(3, ["яблоко", "яблока", "яблок"])).toBe("яблока");
+  });
+  it("8 яблок", () => {
+    expect(declOfNum(8, ["яблоко", "яблока", "яблок"])).toBe("яблок");
+  });
 });
+
