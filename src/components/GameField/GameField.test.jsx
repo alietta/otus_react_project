443a44773931936f605d1659e85162136df7ea67
Tatiana Kudashova <kudashova93@gmail.com>
2020-04-19 18:58:09 +0400

    add test

---
+++ src/components/GameField/GameField.test.jsx
@@ -0,0 +1,41 @@
+/**
+ * @jest-environment jsdom
+*/
+import React from "react";
+import { mount, configure } from "enzyme";
+import renderer from "react-test-renderer";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
+
+import { GameField } from "./GameField";
+
+describe("GameField", () => {
+  it("renders cells for passed empty GameField", () => {
+    expect(
+      renderer
+        .create(
+          <GameField
+            field={[
+              [false, false],
+              [false, false],
+            ]}
+          />
+        )
+        .toJSON()
+    ).toMatchSnapshot();
+  });
+  it("renders filled cells snapshot check", () => {
+    expect(
+      renderer
+        .create(
+          <GameField
+            field={[
+              [true, false],
+              [false, false],
+            ]}
+          />
+        )
+        .toJSON()
+    ).toMatchSnapshot();
+  });
+});
--- src/components/GameField/GameField.tsx
+++ src/components/GameField/GameField.tsx
@@ -10,7 +10,7 @@ interface GameFildProps {
 
 const GameField: FunctionComponent<GameFildProps> = (props) => {
   const [field, setField] = useState<Array<boolean>>(props.field);
-  const onClick = (x: number, y: number, isFilled?: boolean = false): void => {
+  const onClick = (x: number, y: number, isFilled? = false): void => {
     const isXValid = x >= 0 && x < field[0].length;
     const isYValid = y >= 0 && y < field.length;
     const areCoordinatesValid = isXValid && isYValid;
@@ -21,7 +21,7 @@ const GameField: FunctionComponent<GameFildProps> = (props) => {
     const fieldStateCopy = field.map((row) => [...row]);
     fieldStateCopy[y][x] = isFilled;
     setField(fieldStateCopy);
-  }
+  };
 
   return (
     <Field>
@@ -29,7 +29,15 @@ const GameField: FunctionComponent<GameFildProps> = (props) => {
         return (
           <div key={`row-${index}`}>
             {arr.map((isFilled, ind) => {
-              return <Cell key={`y-${index}_x-${ind}`} isFilled={isFilled} x={ind} y={index} onClick={onClick}/>;
+              return (
+                <Cell
+                  key={`y-${index}_x-${ind}`}
+                  isFilled={isFilled}
+                  x={ind}
+                  y={index}
+                  onClick={onClick}
+                />
+              );
             })}
           </div>
         );
