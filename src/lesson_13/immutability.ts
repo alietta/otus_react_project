f9c9790d55b2bb7586e6d32523bf8c307cf3f9d0
Tatiana Kudashova <kudashova93@gmail.com>
2020-05-30 14:40:19 +0400

    immutability part

--- src/lesson_13/immutability.ts
+++ src/lesson_13/immutability.ts
@@ -14,14 +14,22 @@ export type ExpectedTeam = {
 export const originalTeamToExpectedTeam = (
   originalTeam: OriginalTeam
 ): ExpectedTeam => {
-  //
+  const changes = { name: "New York Badgers", roster: 25 };
+  const result = { ...originalTeam, ...changes }
+  delete result.size;
+  return result;
 };
 
 // Задание 2
 type SomeArray = Array<number | string>;
 
-const originalArrayToExpectedArray = (originalArray: SomeArray): SomeArray => {
-  //
+export const originalArrayToExpectedArray = (
+  originalArray: SomeArray
+): SomeArray => {
+  const copy = [...originalArray];
+  copy.splice(0, 2, "two");
+  copy.push(5);
+  return copy;
 };
 
 // Задание 3
@@ -34,6 +42,22 @@ export type Team = {
   };
 };
 
-export const originalTeamToExpectedTeam = (originalTeam: Team): Team => {
-  //
-}
+export const originalTeamToDeepExpectedTeam = (originalTeam: Team): Team => {
+  const changes = {
+    captain: {
+      age: 28,
+    },
+  };
+  const merge = (changes, origin) => {
+    const result = Object.keys(origin).reduce((acc, key) => {
+      if (origin[key] instanceof Object) {
+        acc[key] = merge(changes[key], origin[key]);
+      } else {
+        acc[key] = changes[key] || origin[key];
+      }
+      return acc;
+    }, {});
+    return result;
+  };
+  return merge(changes, originalTeam);
+};
