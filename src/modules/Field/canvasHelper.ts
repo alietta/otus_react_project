c38cac33dfe127e57c571a1039927ba5b5a3bb52
Tatiana Kudashova <kudashova93@gmail.com>
2020-06-27 20:52:07 +0400

    changes from pr to remove extra code

---
+++ src/modules/Field/canvasHelper.ts
@@ -0,0 +1,51 @@
+export interface Point {
+  x: number;
+  y: number;
+}
+
+interface Settings {
+  strokeStyle?: string;
+  lineWidth?: number;
+}
+
+export const contextSettings = (
+  ctx: CanvasRenderingContext2D,
+  { strokeStyle = "black", lineWidth = 1 }: Settings
+) => {
+  ctx.strokeStyle = strokeStyle;
+  ctx.lineWidth = lineWidth;
+};
+
+interface Grid {
+  ctx: CanvasRenderingContext2D;
+  width: number;
+  height: number;
+  step: number;
+  settings: Settings;
+}
+
+export const drawLine = (
+  ctx: CanvasRenderingContext2D,
+  from: Point,
+  to: Point
+) => {
+  ctx.moveTo(from.x, from.y);
+  ctx.lineTo(to.x, to.y);
+};
+
+export const drawGrid = ({ ctx, width, height, step, settings }: Grid) => {
+  ctx.beginPath();
+  let xPos = 0;
+  while (xPos <= width) {
+    drawLine(ctx, { x: xPos, y: 0 }, { x: xPos, y: height });
+    xPos += step;
+  }
+
+  let yPos = 0;
+  while (yPos <= height) {
+    drawLine(ctx, { x: 0, y: yPos }, { x: width, y: yPos });
+    yPos += step;
+  }
+  contextSettings(ctx, settings);
+  ctx.stroke();
+};
