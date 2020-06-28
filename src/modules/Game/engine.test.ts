36ed5b09f1656786636eeb4e7f28217a17ae6f72
Tatiana Kudashova <kudashova93@gmail.com>
2020-06-28 18:35:54 +0400

    engine test and fix engine

---
+++ src/modules/Game/engine.test.ts
@@ -0,0 +1,62 @@
+import { checkNeighbor, getGeneration } from "./engine";
+import { uniq } from "ramda";
+
+describe("Engine", () => {
+  describe("checkNeighbor", () => {
+    let arr;
+    beforeEach(() => {
+      arr = [
+        [false, true, false],
+        [false, true, false],
+        [false, true, false],
+      ];
+    });
+    it("check neighbor point 0 0", () => {
+      expect(checkNeighbor(arr, 0, 0)).toEqual(2);
+    });
+    it("check neighbor point 1 1", () => {
+      expect(checkNeighbor(arr, 1, 1)).toEqual(2);
+    });
+    it("check neighbor point 0 1", () => {
+      expect(checkNeighbor(arr, 0, 1)).toEqual(3);
+    });
+  });
+  describe("getGeneration", () => {
+    it("getGeneration figure 1", () => {
+      const arr = [
+        [false, true, false],
+        [false, true, false],
+        [false, true, false],
+      ];
+      expect(getGeneration(arr)).toEqual([
+        [false, false, false],
+        [true, true, true],
+        [false, false, false],
+      ]);
+    });
+    it("getGeneration figure 2", () => {
+      const arr = [
+        [true, true, false],
+        [true, true, false],
+        [false, false, false],
+      ];
+      expect(getGeneration(arr)).toEqual([
+        [true, true, false],
+        [true, true, false],
+        [false, false, false],
+      ]);
+    });
+    it("getGeneration figure 3", () => {
+      const arr = [
+        [false, false, false],
+        [true, true, false],
+        [false, false, false],
+      ];
+      expect(getGeneration(arr)).toEqual([
+        [false, false, false],
+        [false, false, false],
+        [false, false, false],
+      ]);
+    });
+  });
+});
