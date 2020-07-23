4852d921978874a2344b67f4a41f314e9f38a7cc
Tatiana Kudashova <kudashova93@gmail.com>
2020-07-23 17:24:23 +0400

    login test e2e

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -7,9 +7,20 @@ describe("Login page", () => {
 
   it("should display login window", async () => {
     await new Promise((res) => {
-      setTimeout(res, 3000);
+      setTimeout(res, 1010);
     });
     const text = await page.evaluate(() => document.body.textContent);
     expect(text).toContain("Sign in to Life Game");
+  });
+
+  it("should log in", async () => {
+    await page.waitFor("input");
+    await page.type("input", "Abby");
+    await page.click("button");
+    await new Promise((res) => {
+      setTimeout(res, 1010);
+    });
+    const text = await page.evaluate(() => document.body.textContent);
+    expect(text).toContain("Abby");
   });
 });
