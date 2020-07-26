ed6123fffa2c4eee0068fd2d7afd2d321ec6c06e
Tatiana Kudashova <kudashova93@gmail.com>
2020-07-26 17:03:19 +0400

    query for photos

--- src/specialLessons/lessonApollo/components/Square/index.tsx
+++ src/specialLessons/lessonApollo/components/Square/index.tsx
@@ -1,6 +1,29 @@
 import React, { FC } from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
 
 const Photos: FC = () => {
-  return <div>Apollo page</div>;
+  return (
+    <div>
+      <Query
+        query={gql`
+          {
+            photos {
+              url
+              author
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error</p>;
+          return data.photos.map((photo) => (
+            <img src={photo.url} alt={photo.author} />
+          ));
+        }}
+      </Query>
+    </div>
+  );
 };
 export { Photos };
